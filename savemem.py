e064e9673e1d6d4d66c3ff5be85b97a3aa92adf3
Aaron Hosford <hosford42@gmail.com>
2015-06-10 15:54:27 -0500

    Revamp
    The previous implementation turned out to be way to slow for the purpose
    it was designed for. I gutted it and used a simple count-based cache
    limit instead of using sys.getsizeof().  I offloaded the flush operation
    to a background thread so the process only waits when it needs to access
    the shelf and the shelf is being written to.

--- savemem.py
+++ savemem.py
@@ -8,30 +8,29 @@
 
 
 __author__ = 'Aaron Hosford'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
-import math
 import os
 import pickle
 import shelve
 import shutil
-import sys
 import tempfile
 import time
+import threading
 
 from collections.abc import MutableMapping, MutableSequence, MutableSet
 
 
-def get_default_max_memory():
-    """Return the default maximum memory per container."""
-    return LowMemContainer._default_max_memory
+def get_default_cache_limit():
+    """Return the default maximum cache size per container."""
+    return LowMemContainer._default_cache_limit
 
-def set_default_max_memory(max_memory):
-    """Set the default maximium memory per container."""
-    assert isinstance(max_memory, int)
-    assert max_memory >= 0
-    LowMemContainer._default_max_memory = max_memory
+def set_default_cache_limit(limit):
+    """Set the default maximum cache limit per container."""
+    assert isinstance(limit, int)
+    assert limit >= 0
+    LowMemContainer._default_cache_limit = limit
 
 
 class LowMemContainer:
@@ -40,33 +39,7 @@ class LowMemContainer:
     threshold.
     """
 
-    _default_max_memory = 100000000
-
-    def __init__(self, *args, **kwargs):
-        self._dir = tempfile.mkdtemp()
-        self._shelf = shelve.open(os.path.join(self._dir, 'lowmemdict'))
-        self._open = True
-        self._max_memory = self._default_max_memory
-        self._contents = dict(*args, **kwargs)
-        self._recency = dict.fromkeys(self._contents, time.time())
-        self._auto_flush = False
-        self._trim_memory()
-
-    def get_memory_usage(self):
-        """Return the total size in bytes of this container. Note that if keys or values are composite objects, only
-        the size of the root-level object, not its members, will be accounted for."""
-        return (
-            sys.getsizeof(self) +
-            sys.getsizeof(self._shelf) +
-            sys.getsizeof(self._open) +
-            sys.getsizeof(self._max_memory) +
-            sys.getsizeof(self._contents) +
-            sys.getsizeof(self._recency) +
-            sys.getsizeof(self._auto_flush) +
-            sum(sys.getsizeof(key) for key in self._contents) +
-            sum(sys.getsizeof(value) for value in self._contents.values()) +
-            sum(sys.getsizeof(value) for value in self._recency.values())
-        )
+    _default_cache_limit = 100000
 
     @staticmethod
     def _encode_key(key):
@@ -84,41 +57,48 @@ class LowMemContainer:
         del key_bytes
         return key
 
+    def __init__(self, *args, **kwargs):
+        self._dir = tempfile.mkdtemp()
+        self._shelf = shelve.open(os.path.join(self._dir, 'lowmemdict'))
+        self._open = True
+
+        self._cache_limit = self._default_cache_limit
+        self._cache = dict(*args, **kwargs)
+        self._recency = {}
+
+        self._flush_thread = None
+        self._stop_flushing = False
+        self._flush_exception = None
+
+        self.flush()
+
+    def __del__(self):
+        self.close()
+
+    @property
+    def cache_limit(self):
+        """The cache limit for this container. This is the maximum number of items (values or key/value pairs)
+        that this container will hold in memory."""
+        return self._cache_limit
+
+    @property
+    def cache_size(self):
+        """The current actual cache size. This will never exceed the cache limit."""
+        return len(self._cache)
+
     @property
     def is_open(self):
         """A Boolean flag indicating whether the container is open. Attempting to use it after it has been closed
         will cause an error to be raised."""
         return self._open
 
-    def _get_auto_flush(self):
-        return self._auto_flush
-
-    def _set_auto_flush(self, value):
-        assert value in (0, 1)
-        self._auto_flush = bool(value)
-
-    auto_flush = property(
-        _get_auto_flush,
-        _set_auto_flush,
-        doc="A Boolean flag indicating whether flush() should automatically be called when memory "
-            "usage reaches its max."
-    )
-
-    def __del__(self):
-        self.close()
-
     def close(self):
         """Close the container. Attempting to use it after it has been closed will cause an error to be raised."""
         if not self._open:
             return
 
-        if self._contents is not None:
-            self._contents.clear()
-            self._contents = None
-
-        if self._recency is not None:
-            self._recency.clear()
-            self._recency = None
+        # Close out the flush thread, if necessary.
+        self._wait_for_flush(interrupt=True)
 
         if self._shelf is not None:
             self._shelf.close()
@@ -128,131 +108,138 @@ class LowMemContainer:
             shutil.rmtree(self._dir)
             self._dir = None
 
-        self._max_memory = None
+        if self._cache is not None:
+            self._cache.clear()
+            self._cache = None
+
+        if self._recency is not None:
+            self._recency.clear()
+            self._recency = None
+
         self._open = False
 
-    def flush(self):
+    def _wait_for_flush(self, interrupt=False):
+        if interrupt:
+            self._stop_flushing = True
+
+        # If there is already a flush in progress, wait for it to complete.
+        if self._flush_thread:
+            if self._flush_thread.is_alive() and not self._flush_thread is threading.current_thread():
+                self._flush_thread.join()
+            self._flush_thread = None
+
+        self._stop_flushing = False
+
+        if self._flush_exception:
+            flush_exception = self._flush_exception
+            self._flush_exception = None
+            raise flush_exception
+
+    def _do_flush(self, cache):
+        """Moves contents of the given cache dict to disk."""
+        try:
+            while cache and not self._stop_flushing:
+                key, value = cache.popitem()
+                self._shelf[self._encode_key(key)] = value
+            if cache:
+                cache.clear()
+        except BaseException as exception:
+            self._flush_exception = exception
+
+    def flush(self, synchronous=False, exclusions=None):
         """Completely remove all cached items from RAM and dump them to disk."""
-        self._trim_memory(0)
+        if not self._cache:
+            return  # Nothing to do
 
-    def _get_max_memory(self):
-        return self._max_memory
+        self._wait_for_flush()
 
-    def _set_max_memory(self, max_memory):
-        self._max_memory = max_memory
-        self._trim_memory()
+        cache = self._cache
 
-    max_memory = property(
-        _get_max_memory,
-        _set_max_memory,
-        doc="The maximum amount of RAM the container is permitted to use."
-    )
-
-    def _trim_memory(self, max_memory=None):
-        """Moves contents out of RAM and on to disk until the maximum memory threshold is met."""
-        if max_memory is None:
-            max_memory = self._max_memory
-
-        assert max_memory >= 0
-
-        # If we are completely clearing the cache, there is a faster way.
-        if max_memory == 0 or (self._auto_flush and self.get_memory_usage() > self._max_memory):
+        if exclusions:
+            recency = self._recency
+            self._recency = {}
+            for key in exclusions:
+                self._cache[key] = cache.pop(key)
+                self._recency[key] = recency.pop(key)
+            del recency
+        else:
+            self._cache = {}
             self._recency.clear()
-            while self._contents:
-                key, value = self._contents.popitem()
-                key_str = self._encode_key(key)
-                del key
-                self._shelf[key_str] = value
-                del key_str
-                del value
-            return
 
-        # Otherwise, estimate how much we need to remove, remove it, and then check again until
-        # it is below the threshold.
-        current_size = self.get_memory_usage()
-        while self._contents and current_size > max_memory:
-            fraction_to_remove = (current_size - max_memory) / current_size
-            number_to_remove = math.ceil(fraction_to_remove * len(self._contents))
-            for _ in range(number_to_remove):
-                # Move the least recently used item out of memory.
-                lru_key = min(self._recency, key=self._recency.get)
-                lru_value = self._contents.pop(lru_key)
-                del self._recency[lru_key]
+        self._flush_thread = threading.Thread(target=self._do_flush, args=(cache,))
+        self._flush_thread.daemon = True
+        self._flush_thread.start()
 
-                # And on to disk
-                key_str = self._encode_key(lru_key)
-                self._shelf[key_str] = lru_value
-
-                del key_str
-                del lru_value
-                del lru_key
-            current_size = self.get_memory_usage()
+        if synchronous:
+            del cache
+            self._wait_for_flush()
 
     def _get(self, key):
-        if key in self._contents:
-            value = self._contents[key]
+        if key in self._cache:
             self._recency[key] = time.time()
-            del key
-        else:
-            key_str = self._encode_key(key)
-            try:
-                value = self._shelf.pop(key_str)
-            except KeyError:
-                raise KeyError(key)
-            del key_str
-            self._contents[key] = value
-            self._recency[key] = time.time()
-            self._trim_memory()
-            del key
+            return self._cache[key]
+
+        self._wait_for_flush()
+
+        try:
+            value = self._shelf[self._encode_key(key)]
+        except KeyError:
+            raise KeyError(key)
+
+        if len(self._cache) >= self._cache_limit:
+            self.flush()
+
+        self._cache[key] = value
+        self._recency[key] = time.time()
+
         return value
 
     def _set(self, key, value):
-        if key in self._contents:
-            old_value = self._contents[key]
-            self._contents[key] = value
-            self._recency[key] = time.time()
-            if sys.getsizeof(old_value) < sys.getsizeof(value):
-                self._trim_memory()
-            del key, old_value, value
-        else:
-            key_str = self._encode_key(key)
-            if key_str in self._shelf:
-                del self._shelf[key_str]
-            del key_str
-            self._contents[key] = value
-            self._recency[key] = time.time()
-            self._trim_memory()
-            del key, value
+        if key in self._cache:
+            self._cache[key] = value
+            return
+
+        if len(self._cache) >= self._cache_limit:
+            self.flush()
+
+        self._cache[key] = value
+        self._recency[key] = time.time()
 
     def _del(self, key):
-        if key in self._contents:
-            del self._contents[key]
+        if key in self._cache:
+            del self._cache[key]
             del self._recency[key]
-        else:
-            key_str = self._encode_key(key)
-            try:
-                del self._shelf[key_str]
-            except KeyError:
-                raise KeyError(key)
 
-    def __contains__(self, item):
-        if item in self._contents:
+        self._wait_for_flush()
+
+        try:
+            del self._shelf[self._encode_key(key)]
+        except KeyError:
+            pass
+
+    def __contains__(self, key):
+        if key in self._cache:
+            self._recency[key] = time.time()
             return True
-        key_str = self._encode_key(item)
-        return key_str in self._shelf
+
+        if key in self._recency:
+            return True
+
+        self._wait_for_flush()
+        return self._encode_key(key) in self._shelf
 
     def __iter__(self):
-        for key in self._contents:
-            yield key
+        self._wait_for_flush()
         for key_str in self._shelf:
             yield self._decode_key(key_str)
 
     def __len__(self):
-        return len(self._contents) + len(self._shelf)
+        return len(self._shelf)
 
     def clear(self):
         """Remove all items."""
-        self._contents.clear()
+        self._wait_for_flush(interrupt=True)
+        self._cache.clear()
         self._recency.clear()
         self._shelf.clear()
 
@@ -269,8 +256,10 @@ class LowMemSet(LowMemContainer, MutableSet):
 
     def discard(self, item):
         """Remove an item from the set. If it is not present, do nothing."""
-        if item in self:
+        try:
             self._del(item)
+        except KeyError:
+            pass
 
 
 class LowMemMultiSet(LowMemContainer, MutableSet):
@@ -288,17 +277,18 @@ class LowMemMultiSet(LowMemContainer, MutableSet):
 
     def discard(self, item):
         """Remove an item from the set. If it is not present, do nothing."""
-        if item in self:
+        try:
             counter = self._get(item) - 1
-            if counter <= 0:
-                self._del(item)
-            else:
-                self._set(item, counter)
+        except KeyError:
+            return
+
+        if counter <= 0:
+            self._del(item)
+        else:
+            self._set(item, counter)
 
     def __iter__(self):
-        for key, count in self._contents.items():
-            for _ in range(count):
-                yield key
+        self._wait_for_flush()
         for key_str, count in self._shelf.items():
             key = self._decode_key(key_str)
             for _ in range(count):
@@ -336,6 +326,7 @@ class LowMemList(LowMemContainer, MutableSequence):
         # TODO: Figure out a good way to work around this sort of thing without resorting to rewriting the entire DB.
         if isinstance(key, slice):
             raise NotImplementedError("Slices are not supported.")
+
         index = self._convert_index(key)
         if index == len(self) - 1:
             self._del(index)
@@ -365,12 +356,7 @@ class LowMemDict(LowMemContainer, MutableMapping):
     threshold is 10000 bytes.
     """
 
-    def __getitem__(self, key):
-        return self._get(key)
-
-    def __setitem__(self, key, value):
-        self._set(key, value)
-
-    def __delitem__(self, key):
-        self._del(key)
+    __getitem__ = LowMemContainer._get
+    __setitem__ = LowMemContainer._set
+    __delitem__ = LowMemContainer._del
 
